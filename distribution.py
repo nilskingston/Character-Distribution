2f711a1029b1b0b98e3831723da087e58d65a778
nilskingston <nilskingston@hanovernorwichschools.org>
2015-09-30 13:41:46 -0400

    Updated from Brython Server: 9/30/2015 1:41:46 PM

--- distribution.py
+++ distribution.py
@@ -36,6 +36,10 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
+import string
+a = string.ascii_lowercase
+l = input("Please enter a string of text (the bigger the better) ")
+
 counts = []
 for i in list(a):
     counts.append(l.count(i))
